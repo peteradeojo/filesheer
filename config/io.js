df12a11d01f998a6725780d481751c9fa199e024
Peter Ade-Ojo <adeojopeter@gmail.com>
2022-11-02 15:05:49 +0100

    ui updates

--- config/io.js
+++ config/io.js
@@ -5,22 +5,31 @@ module.exports = (io) => {
 
 	io.on('connect', (socket) => {
 		socket.on('register', (username) => {
+			const room = rooms.find((roomy) => roomy.username === username);
+			if (room.count >= 16) {
+				socket.emit('fail', { message: 'Too many users on this channel' });
+				return undefined;
+			}
+
+			if (!room) {
+				rooms.push({ username, count: 1 });
+			} else {
+				room.count += 1;
+			}
+
 			rooms.forEach((room) => {
 				if (room.username !== username) {
 					socket.leave(room.username);
 				}
 			});
 
-			const room = rooms.find((roomy) => roomy.username === username);
-			if (!room) {
-				rooms.push({ username });
-			}
 			socket.join(username);
 			socket.emit('registered', username);
 		});
 
 		socket.on('share', (state, data) => {
 			if (!state.username) {
+				socket.emit('fail', { message: 'Invalid username' });
 			}
 			const { username } = rooms.find((room) => room.username === state.username);
 
